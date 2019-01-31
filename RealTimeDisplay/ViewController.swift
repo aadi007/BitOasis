74ad087281ca710086d8335d7e81517a3e80ed5b
Aadesh Maheshwari <amaheshwari@aadeshs-mac.local>
2019-01-31 14:53:56 +0400

    Initial Commit

---
+++ RealTimeDisplay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealTimeDisplay
+//
+//  Created by Aadesh Maheshwari on 1/31/19.
+//  Copyright © 2019 Aadesh Maheshwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
