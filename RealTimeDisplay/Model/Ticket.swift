b84aa0af3f15c78d490f9aa13eaf561310f76572
Aadesh Maheshwari <amaheshwari@Aadeshs-mac.local>
2019-02-01 13:12:33 +0400

    login sign up feature added

--- RealTimeDisplay/Model/Ticket.swift
+++ RealTimeDisplay/Model/Ticket.swift
@@ -8,6 +8,28 @@
 
 import UIKit
 
-class Ticket: NSObject {
+class Ticker {
+    var channelId = 0
+    var tickerData: TickerData
+    init(input: [Any]) {
+        if input.count > 3 {
+            
+        }
+    }
+}
+
+class TickerData {
+    var currencyPairId = 0
+    var lastTradePrice = "0"
+    var lowestAsk = "0"
+    var highestBid = "0"
+    var percentChangeIn24Hours = "0"
+    var baseCurrencyVolumeIn24hours = "0"
+    var isFrozen = false
+    var highestPriceIn24Hours = "0"
+    var lowestPriceIn24Hours = "0"
     
+    init(input: [Any]) {
+        
+    }
 }
